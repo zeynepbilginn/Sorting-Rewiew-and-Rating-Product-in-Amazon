8e86f0e92773f079001811b85d91d389bdc2f509
Zeynep Bilgin <101410658+zeynepbilginn@users.noreply.github.com>
2024-02-12 13:19:17 +0300

    Add files via upload

---
+++ Rating Product & Sorting Reviews in Amazon.py
@@ -0,0 +1,135 @@
+###################################################
+# PROJE: Rating Product & Sorting Reviews in Amazon
+###################################################
+
+###################################################
+# İş Problemi
+###################################################
+
+# E-ticaretteki en önemli problemlerden bir tanesi ürünlere satış sonrası verilen puanların doğru şekilde hesaplanmasıdır.
+# Bu problemin çözümü e-ticaret sitesi için daha fazla müşteri memnuniyeti sağlamak, satıcılar için ürünün öne çıkması ve satın
+# alanlar için sorunsuz bir alışveriş deneyimi demektir. Bir diğer problem ise ürünlere verilen yorumların doğru bir şekilde sıralanması
+# olarak karşımıza çıkmaktadır. Yanıltıcı yorumların öne çıkması ürünün satışını doğrudan etkileyeceğinden dolayı hem maddi kayıp
+# hem de müşteri kaybına neden olacaktır. Bu 2 temel problemin çözümünde e-ticaret sitesi ve satıcılar satışlarını arttırırken müşteriler
+# ise satın alma yolculuğunu sorunsuz olarak tamamlayacaktır.
+
+###################################################
+# Veri Seti Hikayesi
+###################################################
+
+# Amazon ürün verilerini içeren bu veri seti ürün kategorileri ile çeşitli metadataları içermektedir.
+# Elektronik kategorisindeki en fazla yorum alan ürünün kullanıcı puanları ve yorumları vardır.
+
+# Değişkenler:
+# reviewerID: Kullanıcı ID’si
+# asin: Ürün ID’si
+# reviewerName: Kullanıcı Adı
+# helpful: Faydalı değerlendirme derecesi
+# reviewText: Değerlendirme
+# overall: Ürün rating’i
+# summary: Değerlendirme özeti
+# unixReviewTime: Değerlendirme zamanı
+# reviewTime: Değerlendirme zamanı Raw
+# day_diff: Değerlendirmeden itibaren geçen gün sayısı
+# helpful_yes: Değerlendirmenin faydalı bulunma sayısı
+# total_vote: Değerlendirmeye verilen oy sayısı
+
+
+###################################################
+# GÖREV 1: Average Rating'i Güncel Yorumlara Göre Hesaplayınız ve Var Olan Average Rating ile Kıyaslayınız.
+###################################################
+
+# Paylaşılan veri setinde kullanıcılar bir ürüne puanlar vermiş ve yorumlar yapmıştır.
+# Bu görevde amacımız verilen puanları tarihe göre ağırlıklandırarak değerlendirmek.
+# İlk ortalama puan ile elde edilecek tarihe göre ağırlıklı puanın karşılaştırılması gerekmektedir.
+
+import pandas as pd
+import math
+import scipy.stats as st
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.expand_frame_repr', False)
+pd.set_option('display.float_format', lambda x: '%.5f' % x)
+
+###################################################
+# Adım 1: Veri Setini Okutunuz ve Ürünün Ortalama Puanını Hesaplayınız.
+###################################################
+df = pd.read_csv("Miuul_Measurement/Datasets/amazon_review.csv")
+df.head(100)
+df.info()
+df.describe()
+df["overall"].mean()  # 4.587589013224822
+
+df["total_vote"].sort_values(ascending=False).head(20)
+###################################################
+# Adım 2: Tarihe Göre Ağırlıklı Puan Ortalamasını Hesaplayınız.
+###################################################
+df["reviewTime"] = pd.to_datetime(df["reviewTime"])
+
+
+def time_based_weighted_average(dataframe, w1=28, w2=26, w3=24, w4=22):
+    return dataframe.loc[df["day_diff"] <= 30, "overall"].mean() * w1 / 100 + \
+        dataframe.loc[(dataframe["day_diff"] > 30) & (dataframe["day_diff"] <= 90), "overall"].mean() * w2 / 100 + \
+        dataframe.loc[(dataframe["day_diff"] > 90) & (dataframe["day_diff"] <= 180), "overall"].mean() * w3 / 100 + \
+        dataframe.loc[(dataframe["day_diff"] > 180), "overall"].mean() * w4 / 100
+
+
+time_based_weighted_average(df)  # 4.6987161061560725
+
+###################################################
+# Görev 2: Ürün için Ürün Detay Sayfasında Görüntülenecek 20 Review'i Belirleyiniz.
+###################################################
+
+###################################################
+# Adım 1. helpful_no Değişkenini Üretiniz
+###################################################
+
+# Not:
+# total_vote bir yoruma verilen toplam up-down sayısıdır.
+# up, helpful demektir.
+# veri setinde helpful_no değişkeni yoktur, var olan değişkenler üzerinden üretilmesi gerekmektedir.
+
+df["helpful_no"] = df["total_vote"] - df["helpful_yes"]
+
+
+###################################################
+# Adım 2. score_pos_neg_diff, score_average_rating ve wilson_lower_bound Skorlarını Hesaplayıp Veriye Ekleyiniz
+###################################################
+
+def score_up_down_diff(up, down):
+    return up - down
+
+
+def score_average_rating(up, down):
+    if up + down == 0:
+        return 0
+    return up / (up + down)
+
+
+def wilson_lower_bound(up, down, confidence=0.95):
+    n = up + down
+    if n == 0:
+        return 0
+    z = st.norm.ppf(1 - (1 - confidence) / 2)
+    phat = 1.0 * up / n
+    return (phat + z * z / (2 * n) - z * math.sqrt((phat * (1 - phat) + z * z / (4 * n)) / n)) / (1 + z * z / n)
+
+df["score_up_down_diff"] = df.apply(lambda x: score_up_down_diff(x["helpful_yes"],x["helpful_no"]),axis=1)
+
+df["score_average_rating_score"] = df.apply(lambda x: score_average_rating(x["helpful_yes"],x["helpful_no"]), axis=1)
+
+df["wilson_review_score"] = df.apply(lambda x: wilson_lower_bound(x["helpful_yes"], x["helpful_no"]))
+
+
+##################################################
+# Adım 3. 20 Yorumu Belirleyiniz ve Sonuçları Yorumlayınız.
+###################################################
+df.sort_values("wilson_review_score",ascending=False).head(20)
+
+
+
+
+
+
+
+
